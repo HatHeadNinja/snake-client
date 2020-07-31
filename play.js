5b48bfc5741b1936f66c5532a504d20c84943099
Doug Dyson <dougdyson@gmail.com>
2020-07-31 03:15:37 +0000

    Disconnect and exit process

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 // play.js
 const { connect } = require('./client');
 console.log('Connecting ...');
-connect();
+const conn = connect();
 
 /**
  * Setup User Interface 
@@ -12,5 +12,17 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  stdin.on('data', handleUserInput);
   return stdin;
 }
+
+// disconnect and exit process on ^C
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input
+  console.log(key);
+  if (key === '\u0003') {
+    conn.disconnect();
+    process.exit();
+  }
+}
+
