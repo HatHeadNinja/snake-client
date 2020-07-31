bbc65fcc1271ac64f16a2b1d97e8c4f0bdbc497d
Doug Dyson <dougdyson@gmail.com>
2020-07-31 19:59:50 +0000

    Completed WASD snake movement

--- input.js
+++ input.js
@@ -2,7 +2,11 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+let connection; 
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,14 +15,27 @@ const setupInput = function() {
   return stdin;
 }
 
-// disconnect and exit process on ^C
 const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
-  console.log(key);
+  // console.log(key);
   if (key === '\u0003') {
     //conn.disconnect();
     process.exit();
   }
+  if (key === 'w'){
+    connection.write("Move: up");
+    //console.log("\033[F");
+  }
+  if (key === 'a'){
+    connection.write("Move: left");
+  }
+  if (key === 's'){
+    connection.write("Move: down");
+  }
+  if (key === 'd'){
+    connection.write("Move: right");
+  }
+  
 }
 
 module.exports = {setupInput};
